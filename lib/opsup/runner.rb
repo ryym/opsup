851b67f8eee18a999473c794bb5a1fd50ad861ac
ryym <ryym.64@gmail.com>
2019-07-07 13:38:23 +0900

    feat: Verify specified stack exists

--- lib/opsup/runner.rb
+++ lib/opsup/runner.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'aws-sdk-opsworks'
+
 module Opsup
   class Runner
     private_class_method :new
@@ -29,6 +31,13 @@ module Opsup
       validate_commands(commands)
 
       @logger.debug("Running #{commands} with #{config.to_h}")
+
+      opsworks = new_opsworks_client(config)
+
+      @logger.debug('Verifying the specified stack exists...')
+      stacks = opsworks.describe_stacks.stacks
+      target_stack = stacks.find { |s| s.name == config.stack_name }
+      raise Opsup::Error, "Stack #{config.stack_name} does not exist" if target_stack.nil?
     end
 
     private def validate_commands(commands)
@@ -36,6 +45,11 @@ module Opsup
 
       unknown_cmds = commands - AVAILABLE_COMMANDS
       raise Opsup::Error, "Unknown commands: #{unknown_cmds.join(' ')}" unless unknown_cmds.empty?
+    end
+
+    private def new_opsworks_client(config)
+      creds = Aws::Credentials.new(config.aws_access_key_id, config.aws_secret_access_key)
+      Aws::OpsWorks::Client.new(region: config.opsworks_region, credentials: creds)
     end
   end
 end
