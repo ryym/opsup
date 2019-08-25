afea63d384e94bc588806c6e97be43445037e0d9
ryym <ryym.64@gmail.com>
2019-08-25 11:45:40 +0900

    refactor(sorbet): Define type annotations :tada:

--- lib/opsup/app.rb
+++ lib/opsup/app.rb
@@ -1,33 +1,40 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require 'aws-sdk-opsworks'
 
 module Opsup
   class App
-    private_class_method :new
+    extend T::Sig
 
+    sig { returns(Opsup::App) }
     def self.create
       new(
         logger: Opsup::Logger.instance,
       )
     end
 
+    sig { params(logger: ::Logger).void }
     def initialize(logger:)
-      @logger = logger
+      @logger = T.let(logger, ::Logger)
     end
 
-    AVAILABLE_COMMANDS = %w[
-      update_cookbooks
-      setup
-      configure
-      deploy
-    ].freeze
+    AVAILABLE_COMMANDS = T.let(
+      %w[
+        update_cookbooks
+        setup
+        configure
+        deploy
+      ].freeze,
+      T::Array[String],
+    )
 
+    sig { returns(T::Array[String]) }
     def available_commands
       AVAILABLE_COMMANDS
     end
 
+    sig { params(commands: T::Array[String], config: Opsup::Config).void }
     def run(commands, config)
       validate_commands(commands)
       @logger.warn('Started in DRYRUN MODE') if config.dryrun
@@ -47,6 +54,7 @@ module Opsup
       @logger.warn('Finished in DRYRUN MODE') if config.dryrun
     end
 
+    sig { params(commands: T::Array[String]).void }
     private def validate_commands(commands)
       raise Opsup::Error, 'No commands specified' if commands.empty?
 
@@ -54,12 +62,14 @@ module Opsup
       raise Opsup::Error, "Unknown commands: #{unknown_cmds.join(' ')}" unless unknown_cmds.empty?
     end
 
+    sig { params(config: Opsup::Config).returns(Aws::OpsWorks::Client) }
     private def new_opsworks_client(config)
       creds = Aws::Credentials.new(config.aws_access_key_id, config.aws_secret_access_key)
       Aws::OpsWorks::Client.new(region: config.opsworks_region, credentials: creds)
     end
 
     # Assumes the command is a valid value.
+    sig { params(command: String).returns(String) }
     private def command_to_opsworks_command(command)
       command == 'update_cookbooks' ? 'update_custom_cookbooks' : command
     end
--- lib/opsup/config.rb
+++ lib/opsup/config.rb
@@ -1,17 +1,40 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Opsup
   class Config
+    extend T::Sig
+
+    sig { returns(String) }
     attr_reader :stack_name
+
+    sig { returns(String) }
     attr_reader :aws_access_key_id
+
+    sig { returns(String) }
     attr_reader :aws_secret_access_key
+
+    sig { returns(String) }
     attr_reader :opsworks_region
+
+    sig { returns(Symbol) }
     attr_reader :running_mode
+
+    sig { returns(T::Boolean) }
     attr_reader :dryrun
 
-    MODES = %i[parallel serial one_then_all].freeze
+    MODES = T.let(%i[parallel serial one_then_all].freeze, T::Array[Symbol])
 
+    sig do
+      params(
+        stack_name: String,
+        aws_access_key_id: String,
+        aws_secret_access_key: String,
+        opsworks_region: String,
+        running_mode: T.nilable(Symbol),
+        dryrun: T::Boolean,
+      ).void
+    end
     def initialize(
       stack_name:,
       aws_access_key_id:,
@@ -20,14 +43,15 @@ module Opsup
       running_mode: nil,
       dryrun: false
     )
-      @stack_name = stack_name
-      @aws_access_key_id = aws_access_key_id
-      @aws_secret_access_key = aws_secret_access_key
-      @opsworks_region = opsworks_region
-      @running_mode = running_mode || MODES[0]
-      @dryrun = dryrun
+      @stack_name = T.let(stack_name, String)
+      @aws_access_key_id = T.let(aws_access_key_id, String)
+      @aws_secret_access_key = T.let(aws_secret_access_key, String)
+      @opsworks_region = T.let(opsworks_region, String)
+      @running_mode = T.let(running_mode || MODES.fetch(0), Symbol)
+      @dryrun = T.let(dryrun, T::Boolean)
     end
 
+    sig { returns(T::Hash[Symbol, T.untyped]) }
     def to_h
       {
         stack_name: stack_name,
